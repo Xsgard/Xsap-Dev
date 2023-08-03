5dea8f29cbba4f0ec531e28241a7ae5c23679455
asgard <1792181243@qq.com>
2023-08-03 10:18:50 +0800

    Index页面的控制器

--- src/main/java/com/kclm/xsap/web/controller/IndexController.java
+++ src/main/java/com/kclm/xsap/web/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 /**
  * @author Asgard
  * @version 1.0
@@ -14,9 +16,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/index")
 public class IndexController {
 
+    /**
+     * 跳转至Index页面
+     *
+     * @return index页面
+     */
     @GetMapping
     public String toIndex() {
         return "index";
     }
 
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/user/login";
+    }
 }
