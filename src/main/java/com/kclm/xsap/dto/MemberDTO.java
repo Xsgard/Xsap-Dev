86804ccb6ccd2b0ea478b8866e6b6be52fb04dcf
asgard <1792181243@qq.com>
2023-08-04 08:58:27 +0800

    此类用来描述了会员携带的各种数据

--- src/main/java/com/kclm/xsap/dto/MemberDTO.java
+++ src/main/java/com/kclm/xsap/dto/MemberDTO.java
@@ -1,26 +1,26 @@
 package com.kclm.xsap.dto;
 
-import java.time.LocalDate;
-import java.util.List;
-
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
- *
  * @author harima
- * @since JDK11.0
  * @CreateDate 2020年9月17日 下午12:03:51
  * @description 此类用来描述了会员携带的各种数据
- *
+ * @since JDK11.0
  */
 @Data
-@EqualsAndHashCode(callSuper=false)
-public class MemberDTO{
+@EqualsAndHashCode(callSuper = false)
+public class MemberDTO {
 
     private Long id;
     /**
-     *  会员名称
+     * 会员名称
      */
     private String name;
 
@@ -34,6 +34,9 @@ public class MemberDTO{
      */
     private String phone;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime joiningDate;
+
     /**
      * 出生日期
      */
@@ -44,6 +47,10 @@ public class MemberDTO{
      */
     private String note;
 
+    /**
+     * 会员卡名
+     */
+    private List<String> cardNameList;
     /**
      * 会员卡信息
      */
