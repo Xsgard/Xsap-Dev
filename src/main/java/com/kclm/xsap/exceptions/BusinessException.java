95637b4ef43d1de7d8bc8a2bd472f18608ea68ad
asgard <1792181243@qq.com>
2023-08-04 19:35:42 +0800

    自定义业务异常

--- src/main/java/com/kclm/xsap/exceptions/BusinessException.java
+++ src/main/java/com/kclm/xsap/exceptions/BusinessException.java
@@ -1,13 +1,40 @@
 package com.kclm.xsap.exceptions;
 
+import lombok.Getter;
+
 /**
  * @author Asgard
  * @version 1.0
  * @description: 自定义业务异常
  * @date 2023/8/3 8:54
  */
+@Getter
 public class BusinessException extends RuntimeException {
+    private String msg;
+    private Integer code = 500;
+
     public BusinessException(String message) {
         super(message);
+        this.msg = message;
     }
+
+    public BusinessException(Integer code, String message) {
+        super(message);
+        this.msg = message;
+        this.code = code;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+
 }
