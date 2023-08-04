82d6aae5fff50d18049111ab42a8e87eb6acd68c
asgard <1792181243@qq.com>
2023-08-04 15:34:57 +0800

    CourseController

---
+++ src/main/java/com/kclm/xsap/web/controller/CourseController.java
@@ -0,0 +1,45 @@
+package com.kclm.xsap.web.controller;
+
+import com.kclm.xsap.entity.CourseEntity;
+import com.kclm.xsap.service.CourseService;
+import com.kclm.xsap.service.MemberCardService;
+import com.kclm.xsap.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * @author Asgard
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/8/4 13:17
+ */
+@RequestMapping("/course")
+@Controller
+public class CourseController {
+
+    private CourseService courseService;
+
+    private MemberCardService cardService;
+
+    @Autowired
+    private void setApplicationContext(CourseService courseService,
+                                       MemberCardService cardService) {
+        this.courseService = courseService;
+        this.cardService = cardService;
+    }
+
+
+    @PostMapping("/courseList.do")
+    @ResponseBody
+    public R getCourseList(Model model) {
+        List<CourseEntity> courseEntities = courseService.list();
+
+        return new R().put("data", courseEntities);
+    }
+}
