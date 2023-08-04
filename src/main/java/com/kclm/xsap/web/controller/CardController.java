776b6f107fed0125ff6b8f22bb4118d1bf76bc36
asgard <1792181243@qq.com>
2023-08-04 15:34:48 +0800

    CardController

--- src/main/java/com/kclm/xsap/web/controller/CardController.java
+++ src/main/java/com/kclm/xsap/web/controller/CardController.java
@@ -2,12 +2,15 @@ package com.kclm.xsap.web.controller;
 
 import com.kclm.xsap.entity.MemberBindRecordEntity;
 import com.kclm.xsap.entity.MemberCardEntity;
+import com.kclm.xsap.service.CourseCardService;
 import com.kclm.xsap.service.MemberBindRecordService;
 import com.kclm.xsap.service.MemberCardService;
 import com.kclm.xsap.service.MemberService;
 import com.kclm.xsap.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,24 +28,49 @@ import java.util.List;
 public class CardController {
     private MemberService memberService;
 
-    private MemberCardService cardService;
+    private MemberCardService memberCardService;
+
+    private CourseCardService courseCardService;
 
     private MemberBindRecordService bindRecordService;
 
     @Autowired
     private void setApplicationContext(MemberService memberService,
                                        MemberCardService cardService,
-                                       MemberBindRecordService bindRecordService) {
+                                       MemberBindRecordService bindRecordService,
+                                       CourseCardService courseCardService) {
+        //
         this.memberService = memberService;
-        this.cardService = cardService;
+        this.memberCardService = cardService;
         this.bindRecordService = bindRecordService;
+        this.courseCardService = courseCardService;
+    }
+
+    @GetMapping("/x_member_card.do")
+    public String toMemberCard() {
+        return "member/x_member_card";
+    }
+
+    @GetMapping("/x_member_add_card.do")
+    public String toMemberAddCard() {
+        return "member/x_member_add_card";
     }
 
     @PostMapping("/cardList.do")
     @ResponseBody
     public R cardList() {
-        List<MemberCardEntity> cardEntityList = cardService.list();
-        return new R().put("data", cardEntityList);
+        List<MemberCardEntity> cardEntityList = memberCardService.list();
+        return new R().setData(cardEntityList);
+    }
+
+    @GetMapping("/x_member_card_edit.do")
+    public String getCardInfoById(Long id, Model model) {
+        MemberCardEntity cardEntity = memberCardService.getById(id);
+        List<Long> courseIdList = courseCardService.getCourseIdList(id);
+
+        model.addAttribute("courseCarry", courseIdList);
+        model.addAttribute("cardMsg", cardEntity);
+        return "member/x_member_card_edit";
     }
 
     @PostMapping("/activeOpt.do")
