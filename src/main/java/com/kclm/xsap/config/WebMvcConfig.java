9de96d9dd56461573cfd00d343f27fe02745197e
asgard <1792181243@qq.com>
2023-08-04 10:41:35 +0800

    WebMvcConfig

--- src/main/java/com/kclm/xsap/config/WebMvcConfig.java
+++ src/main/java/com/kclm/xsap/config/WebMvcConfig.java
@@ -5,8 +5,12 @@
 package com.kclm.xsap.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -30,7 +34,30 @@ public class WebMvcConfig implements WebMvcConfigurer {
         log.debug("\n----> 上传的图片映射路径：{}",UPLOAD_IMAGE_URL);
     }
 
+    @Override
+    public Validator getValidator() {
+        LocalValidatorFactoryBean lvfb = new LocalValidatorFactoryBean();
+        //设置属性
+        lvfb.setValidationMessageSource(messageSource());
+        //返回
+        return lvfb;
+    }
 
+    /*****
+     * 用来指定验证时要读取的资源文件
+     * @return
+     */
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        //属性
+//        messageSource.setBasename("beanValidation");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setCacheSeconds(60);
+        messageSource.setAlwaysUseMessageFormat(true);
+        //
+        return messageSource;
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
