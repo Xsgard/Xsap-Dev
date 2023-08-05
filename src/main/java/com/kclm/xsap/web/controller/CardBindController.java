b5a6d2e25d8ba371e8aec6d8c0bdaa4dea533e3d
asgard <1792181243@qq.com>
2023-08-05 10:12:10 +0800

    Controller

--- src/main/java/com/kclm/xsap/web/controller/CardBindController.java
+++ src/main/java/com/kclm/xsap/web/controller/CardBindController.java
@@ -1,6 +1,12 @@
 package com.kclm.xsap.web.controller;
 
+import com.kclm.xsap.dto.BindCardInfoDto;
+import com.kclm.xsap.service.MemberBindRecordService;
+import com.kclm.xsap.service.MemberCardService;
+import com.kclm.xsap.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,8 +20,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CardBindController {
 
+    private MemberBindRecordService bindRecordService;
+
+    private MemberCardService memberCardService;
+
+    @Autowired
+    private void setApplicationContext(MemberBindRecordService bindRecordService,
+                                       MemberCardService memberCardService) {
+        this.bindRecordService = bindRecordService;
+        this.memberCardService = memberCardService;
+    }
+
     @GetMapping("/x_member_card_bind.do")
     public String toCardBind() {
         return "member/x_member_card_bind";
+    }
+
+    public R memberBind(BindingResult bindingResult, BindCardInfoDto infoDto) {
+        //绑定会员卡信息
+        return memberCardService.memberBind(bindingResult, infoDto);
     }
 }
