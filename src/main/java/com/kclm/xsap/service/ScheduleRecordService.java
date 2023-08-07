348c7a93e9eb130dad2f7d24f166f6bf9b3f7144
asgard <1792181243@qq.com>
2023-08-07 16:20:26 +0800

    Service

--- src/main/java/com/kclm/xsap/service/ScheduleRecordService.java
+++ src/main/java/com/kclm/xsap/service/ScheduleRecordService.java
@@ -2,6 +2,10 @@ package com.kclm.xsap.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.kclm.xsap.entity.ScheduleRecordEntity;
+import com.kclm.xsap.utils.R;
+import org.springframework.validation.BindingResult;
+
+import javax.validation.Valid;
 
 /**
  * @author Asgard
@@ -10,4 +14,5 @@ import com.kclm.xsap.entity.ScheduleRecordEntity;
  * @date 2023/8/2 16:34
  */
 public interface ScheduleRecordService extends IService<ScheduleRecordEntity> {
+    R scheduleAdd(@Valid ScheduleRecordEntity scheduleRecord, BindingResult bindingResult);
 }
--- src/main/java/com/kclm/xsap/service/impl/ScheduleRecordServiceImpl.java
+++ src/main/java/com/kclm/xsap/service/impl/ScheduleRecordServiceImpl.java
@@ -3,8 +3,15 @@ package com.kclm.xsap.service.impl;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.kclm.xsap.dao.ScheduleRecordDao;
 import com.kclm.xsap.entity.ScheduleRecordEntity;
+import com.kclm.xsap.exceptions.BusinessException;
 import com.kclm.xsap.service.ScheduleRecordService;
+import com.kclm.xsap.utils.R;
+import com.kclm.xsap.utils.ValidationUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
 
 /**
  * @author Asgard
@@ -14,4 +21,14 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ScheduleRecordServiceImpl extends ServiceImpl<ScheduleRecordDao, ScheduleRecordEntity> implements ScheduleRecordService {
+
+    @Override
+    public R scheduleAdd(@Valid ScheduleRecordEntity scheduleRecord, BindingResult bindingResult) {
+        ValidationUtil.getErrors(bindingResult);
+        scheduleRecord.setCreateTime(LocalDateTime.now());
+        boolean b = this.save(scheduleRecord);
+        if (!b)
+            throw new BusinessException("课程添加失败");
+        return R.ok();
+    }
 }
