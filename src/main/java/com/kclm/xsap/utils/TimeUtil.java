acaeb36a56b5a1e2ae7ac92a419d5d38286b4a02
asgard <1792181243@qq.com>
2023-08-07 17:12:51 +0800

    时间工具类

---
+++ src/main/java/com/kclm/xsap/utils/TimeUtil.java
@@ -0,0 +1,25 @@
+package com.kclm.xsap.utils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * @author Asgard
+ * @version 1.0
+ * @description: TODO
+ * @date 2023/8/3 19:15
+ */
+public class TimeUtil {
+    public static LocalDateTime timeSub(LocalDateTime time, Integer day) {
+        return time.plusDays(day.longValue());
+    }
+
+    public static LocalDateTime timeTransfer(LocalDate date, LocalTime time) {
+        return LocalDateTime.of(date, time);
+    }
+
+    public static LocalDateTime toEndTime(LocalDateTime start, Long minutes) {
+        return start.plusMinutes(minutes);
+    }
+}
