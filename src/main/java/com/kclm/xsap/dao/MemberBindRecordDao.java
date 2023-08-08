9353b90d45265fd1afee815c14b6a252324c31d7
asgard <1792181243@qq.com>
2023-08-08 13:28:02 +0800

    Dao

--- src/main/java/com/kclm/xsap/dao/MemberBindRecordDao.java
+++ src/main/java/com/kclm/xsap/dao/MemberBindRecordDao.java
@@ -3,6 +3,9 @@ package com.kclm.xsap.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.kclm.xsap.entity.MemberBindRecordEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 /**
  * @author Asgard
@@ -12,4 +15,6 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface MemberBindRecordDao extends BaseMapper<MemberBindRecordEntity> {
+    @Select("select card_id from t_member_bind_record where member_id=#{memberId};")
+    List<Long> getCardIds(Long memberId);
 }
