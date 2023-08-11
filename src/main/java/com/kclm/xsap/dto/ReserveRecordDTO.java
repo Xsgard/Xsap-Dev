ebf40621b1b6602a8cae076e1649822c04a8f9b6
asgard <1792181243@qq.com>
2023-08-11 15:04:33 +0800

    Dto

--- src/main/java/com/kclm/xsap/dto/ReserveRecordDTO.java
+++ src/main/java/com/kclm/xsap/dto/ReserveRecordDTO.java
@@ -1,10 +1,36 @@
 package com.kclm.xsap.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 /**
  * @author Asgard
  * @version 1.0
  * @description: TODO
  * @date 2023/8/2 15:56
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReserveRecordDTO {
+    private String courseName;
+
+    private LocalDateTime reserveTime;
+
+    private String cardName;
+
+    private Integer reserveNumbers;
+
+    private Integer timesCost;
+
+    private LocalDateTime operateTime;
+
+    private String operator;
+
+    private String reserveNote;
+
+    private Integer reserveStatus;
 }
