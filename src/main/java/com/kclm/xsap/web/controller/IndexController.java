4b71418ab62d74f48460efca76578739b4e93302
asgard <1792181243@qq.com>
2023-08-11 19:59:48 +0800

    Controller

--- src/main/java/com/kclm/xsap/web/controller/IndexController.java
+++ src/main/java/com/kclm/xsap/web/controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.kclm.xsap.web.controller;
 
+import com.kclm.xsap.utils.R;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 
@@ -35,5 +38,17 @@ public class IndexController {
     @GetMapping("/x_index_home.do")
     public String indexHome() {
         return "x_index_home";
+    }
+
+    /**
+     * TODO 每月会员新增流失图
+     *
+     * @return R
+     */
+    @PostMapping("/homePageInfo/statisticsOfMemberCard.do")
+    @ResponseBody
+    public R statisticsOfMemberCard() {
+
+        return null;
     }
 }
