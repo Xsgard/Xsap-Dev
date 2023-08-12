e1cc4aa1eed83a9dd9f4864f5e78be348db6d315
asgard <1792181243@qq.com>
2023-08-12 11:54:00 +0800

    Controller

--- src/main/java/com/kclm/xsap/web/controller/IndexController.java
+++ src/main/java/com/kclm/xsap/web/controller/IndexController.java
@@ -1,13 +1,19 @@
 package com.kclm.xsap.web.controller;
 
+import com.kclm.xsap.entity.MemberEntity;
+import com.kclm.xsap.service.IndexPageService;
+import com.kclm.xsap.service.MemberService;
 import com.kclm.xsap.utils.R;
+import com.kclm.xsap.vo.indexStatistics.IndexAddAndStreamInfoVo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * @author Asgard
@@ -18,6 +24,16 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/index")
 public class IndexController {
+
+    private MemberService memberService;
+
+    private IndexPageService indexPageService;
+
+    @Autowired
+    public void setService(MemberService memberService, IndexPageService indexPageService) {
+        this.memberService = memberService;
+        this.indexPageService = indexPageService;
+    }
 
     /**
      * 跳转至Index页面
@@ -45,10 +61,19 @@ public class IndexController {
      *
      * @return R
      */
-    @PostMapping("/homePageInfo/statisticsOfMemberCard.do")
+    @GetMapping("/homePageInfo/statisticsOfNewAndLostPeople.do")
     @ResponseBody
     public R statisticsOfMemberCard() {
+        // 获取当前日期和时间
+        LocalDateTime now = LocalDateTime.now();
+        // 获取本月第一天
+        LocalDateTime firstDayOfMonth = now.withDayOfMonth(1).toLocalDate().atStartOfDay();
+        // 获取今天的最后时间
+        int dayOfMonth = now.getDayOfMonth();
+        LocalDateTime nowDayOfMonth = now.withDayOfMonth(dayOfMonth).toLocalDate().atTime(23, 59, 59);
+        List<MemberEntity> memberEntityList = memberService.filterAllMemberByTime(firstDayOfMonth, nowDayOfMonth);
+        IndexAddAndStreamInfoVo addAndStreamInfo = indexPageService.getAddAndStreamInfo(memberEntityList, firstDayOfMonth, nowDayOfMonth);
 
-        return null;
+        return R.ok().put("data", addAndStreamInfo);
     }
 }
