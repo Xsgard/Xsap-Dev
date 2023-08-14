c774023606b8125ad122bd5b374434a1c4f6e150
asgard <1792181243@qq.com>
2023-08-14 13:14:59 +0800

    Dao

--- src/main/java/com/kclm/xsap/dao/ConsumeRecordDao.java
+++ src/main/java/com/kclm/xsap/dao/ConsumeRecordDao.java
@@ -3,6 +3,7 @@ package com.kclm.xsap.dao;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.kclm.xsap.entity.ConsumeRecordEntity;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 /**
  * @author Asgard
@@ -12,4 +13,6 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface ConsumeRecordDao extends BaseMapper<ConsumeRecordEntity> {
+    @Select("select sum(money_cost) from t_consume_record where member_bind_id=#{memberBindId};")
+    Double getMoneyCostPlus(Long memberBindId);
 }
