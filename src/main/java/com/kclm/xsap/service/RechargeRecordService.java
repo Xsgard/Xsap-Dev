3a9623ce2e4d25a691926744e3636fbaad0be48b
asgard <1792181243@qq.com>
2023-08-15 15:04:21 +0800

    Service

--- src/main/java/com/kclm/xsap/service/RechargeRecordService.java
+++ src/main/java/com/kclm/xsap/service/RechargeRecordService.java
@@ -3,6 +3,9 @@ package com.kclm.xsap.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.kclm.xsap.entity.RechargeRecordEntity;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 /**
  * @author Asgard
  * @version 1.0
@@ -10,4 +13,5 @@ import com.kclm.xsap.entity.RechargeRecordEntity;
  * @date 2023/8/2 16:33
  */
 public interface RechargeRecordService extends IService<RechargeRecordEntity> {
+    List<Integer> getRechargeList(LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/com/kclm/xsap/service/impl/RechargeRecordServiceImpl.java
+++ src/main/java/com/kclm/xsap/service/impl/RechargeRecordServiceImpl.java
@@ -1,10 +1,15 @@
 package com.kclm.xsap.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.kclm.xsap.dao.RechargeRecordDao;
 import com.kclm.xsap.entity.RechargeRecordEntity;
 import com.kclm.xsap.service.RechargeRecordService;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Asgard
@@ -14,4 +19,21 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class RechargeRecordServiceImpl extends ServiceImpl<RechargeRecordDao, RechargeRecordEntity> implements RechargeRecordService {
+
+    @Override
+    public List<Integer> getRechargeList(LocalDateTime start, LocalDateTime end) {
+        LambdaQueryWrapper<RechargeRecordEntity> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.between(RechargeRecordEntity::getCreateTime, start, end);
+        List<RechargeRecordEntity> recharges = this.list(queryWrapper);
+        List<Integer> data = new ArrayList<>();
+        for (int i = 1; i <= end.getMonthValue(); i++) {
+            int finalI = i;
+            Integer monthMoney = (int) recharges.stream()
+                    .filter(e -> e.getCreateTime().getMonthValue() == finalI)
+                    .mapToDouble(item -> item.getReceivedMoney().doubleValue())
+                    .sum();
+            data.set(i - 1, monthMoney);
+        }
+        return data;
+    }
 }
