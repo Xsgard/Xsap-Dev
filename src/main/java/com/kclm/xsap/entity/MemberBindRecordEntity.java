c223e907e4ee6490a6feffdba950bc2289d2f74b
asgard <1792181243@qq.com>
2023-08-18 16:31:32 +0800

    Entity

--- src/main/java/com/kclm/xsap/entity/MemberBindRecordEntity.java
+++ src/main/java/com/kclm/xsap/entity/MemberBindRecordEntity.java
@@ -2,17 +2,16 @@ package com.kclm.xsap.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-
-import java.math.BigDecimal;
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 /**
  * 中间表：会员绑定记录
@@ -25,63 +24,66 @@ import javax.validation.constraints.NotNull;
 @TableName("t_member_bind_record")
 @Accessors(chain = true)
 public class MemberBindRecordEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 *
-	 */
-	@TableId
-	private Long id;
-	/**
-	 * 会员id
-	 */
-	private Long memberId;
-	/**
-	 * 会员卡id
-	 */
-	@NotNull(message = "请选择会员卡！")
-	private Long cardId;
-	/**
-	 * 可使用次数
-	 */
-	@NotNull(message = "请输入要充值的次数")
-	private Integer validCount;
-	/**
-	 * 有效期，按天算
-	 */
-	@NotNull(message = "请填入有效期")
-	private Integer validDay;
-	/**
-	 * 实收金额
-	 */
-	@NotNull(message = "请输入实收金额")
-	private BigDecimal receivedMoney;
-	/**
-	 * 支付方式
-	 */
-	@NotBlank(message = "请选择支付方式")
-	private String payMode;
-	/**
-	 * 备注
-	 */
-	private String note;
-	/**
-	 * 激活状态，1激活，0非激活
-	 */
-	private Integer activeStatus;
-	/**
-	 * 创建时间
-	 */
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	private LocalDateTime createTime;
-	/**
-	 * 修改时间
-	 */
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
-	private LocalDateTime lastModifyTime;
-	/**
-	 * 版本
-	 */
-	private Integer version = 1;
+    /**
+     *
+     */
+    @TableId
+    private Long id;
+    /**
+     * 会员id
+     */
+    private Long memberId;
+    /**
+     * 会员卡id
+     */
+    @NotNull(message = "请选择会员卡！")
+    private Long cardId;
+    /**
+     * 可使用次数
+     */
+    @NotNull(message = "请输入要充值的次数")
+    @Min(value = 0, message = "输入有误，请检查输入次数！")
+    private Integer validCount;
+    /**
+     * 有效期，按天算
+     */
+    @NotNull(message = "请填入有效期")
+    @Min(value = 0, message = "输入有误，请检查输入天数！")
+    private Integer validDay;
+    /**
+     * 实收金额
+     */
+    @NotNull(message = "请输入实收金额")
+    @Min(value = 0, message = "输入有误，请检查输入金额！")
+    private BigDecimal receivedMoney;
+    /**
+     * 支付方式
+     */
+    @NotBlank(message = "请选择支付方式")
+    private String payMode;
+    /**
+     * 备注
+     */
+    private String note;
+    /**
+     * 激活状态，1激活，0非激活
+     */
+    private Integer activeStatus;
+    /**
+     * 创建时间
+     */
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime createTime;
+    /**
+     * 修改时间
+     */
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime lastModifyTime;
+    /**
+     * 版本
+     */
+    private Integer version = 1;
 
 }
