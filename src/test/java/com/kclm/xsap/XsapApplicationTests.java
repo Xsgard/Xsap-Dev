d51d0eee28941ff2d261fb1ae0ceca842f18caad
asgard <1792181243@qq.com>
2023-08-23 17:02:03 +0800

    Test

--- src/test/java/com/kclm/xsap/XsapApplicationTests.java
+++ src/test/java/com/kclm/xsap/XsapApplicationTests.java
@@ -1,18 +1,22 @@
 package com.kclm.xsap;
 
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.LocalDateTimeUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kclm.xsap.dao.CourseCardDao;
 import com.kclm.xsap.dao.MemberCardDao;
 import com.kclm.xsap.dao.MemberDao;
+import com.kclm.xsap.dto.ExportReservationDTO;
 import com.kclm.xsap.dto.MemberDTO;
-import com.kclm.xsap.entity.MemberCardEntity;
-import com.kclm.xsap.entity.MemberEntity;
-import com.kclm.xsap.service.MemberCardService;
-import com.kclm.xsap.service.MemberService;
+import com.kclm.xsap.entity.*;
+import com.kclm.xsap.exceptions.BusinessException;
+import com.kclm.xsap.service.*;
+import com.kclm.xsap.utils.ExcelExportUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -46,6 +50,15 @@ class XsapApplicationTests {
 
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    private ReservationRecordService reservationRecordService;
+
+    @Autowired
+    private ScheduleRecordService scheduleRecordService;
+
+    @Autowired
+    private CourseService courseService;
 
     @Test
     public void testSelect() {
@@ -119,4 +132,33 @@ class XsapApplicationTests {
         System.out.println(Arrays.toString(data.toArray()));
     }
 
+    @Test
+    public void testExportExcel() throws IOException {
+        List<ReservationRecordEntity> recordEntityList = reservationRecordService.list();
+        List<ExportReservationDTO> collect = recordEntityList.stream().map(item -> {
+            ScheduleRecordEntity schedule = scheduleRecordService.getById(item.getScheduleId());
+            CourseEntity course = courseService.getById(schedule.getCourseId());
+            MemberEntity member = memberService.getById(item.getMemberId());
+
+            ExportReservationDTO dto = new ExportReservationDTO();
+            dto.setCourseName(course.getName());
+            dto.setReserveTime(LocalDateTimeUtil.format(item.getCreateTime(), DatePattern.NORM_DATETIME_PATTERN));
+            dto.setMemberName(member.getName());
+            dto.setCardName(item.getCardName());
+            dto.setReserveNumbers(item.getReserveNums());
+            dto.setTimesCost(course.getTimesCost());
+            dto.setOperateTime(item.getLastModifyTime() == null ?
+                    LocalDateTimeUtil.format(item.getCreateTime(), DatePattern.NORM_DATETIME_PATTERN) :
+                    LocalDateTimeUtil.format(item.getLastModifyTime(), DatePattern.NORM_DATETIME_PATTERN));
+            dto.setOperator(item.getOperator());
+            dto.setReserveNote(item.getNote());
+            dto.setReserveStatus(item.getStatus() == 1 ? "有效" : "无效");
+            return dto;
+        }).collect(Collectors.toList());
+
+        boolean b = ExcelExportUtil.excelWriteToLocal(collect);
+        if (!b)
+            throw new BusinessException("导出失败！");
+
+    }
 }
