5c4c05de038564da261580ac2838266209d6f69a
asgard <1792181243@qq.com>
2023-08-23 17:01:44 +0800

    Dto

---
+++ src/main/java/com/kclm/xsap/dto/ExportReservationDTO.java
@@ -0,0 +1,32 @@
+package com.kclm.xsap.dto;
+
+import lombok.Data;
+
+/**
+ * @author Asgard
+ * @version 1.0
+ * @description: TODO 导出预约记录Dto
+ * @date 2023/8/23 15:54
+ */
+@Data
+public class ExportReservationDTO {
+    private String courseName;
+
+    private String reserveTime;
+
+    private String memberName;
+
+    private String cardName;
+
+    private Integer reserveNumbers;
+
+    private Integer timesCost;
+
+    private String operateTime;
+
+    private String operator;
+
+    private String reserveNote;
+
+    private String reserveStatus;
+}
