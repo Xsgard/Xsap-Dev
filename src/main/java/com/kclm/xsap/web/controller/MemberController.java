893152cfbac720c8ca49c8a284be0c343e77e67a
asgard <1792181243@qq.com>
2023-08-24 19:26:36 +0800

    Controller

--- src/main/java/com/kclm/xsap/web/controller/MemberController.java
+++ src/main/java/com/kclm/xsap/web/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.kclm.xsap.web.controller;
 
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.kclm.xsap.dto.MemberCardDTO;
 import com.kclm.xsap.dto.MemberDTO;
 import com.kclm.xsap.dto.ReserveRecordDTO;
@@ -10,8 +11,10 @@ import com.kclm.xsap.service.MemberBindRecordService;
 import com.kclm.xsap.service.MemberCardService;
 import com.kclm.xsap.service.MemberService;
 import com.kclm.xsap.utils.R;
+import com.kclm.xsap.utils.UploadImg;
 import com.kclm.xsap.vo.ClassInfoVo;
 import com.kclm.xsap.vo.ConsumeInfoVo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -31,9 +35,12 @@ import java.util.List;
  * @description: 用于处理/member的请求
  * @date 2023/8/3 10:23
  */
+@Slf4j
 @Controller
 @RequestMapping("/member")
 public class MemberController {
+    //
+    private static final String UPLOAD_IMAGES_MEMBER_IMG = "/upload/images/member_img/";
     private MemberService memberService;
 
     private MemberCardService cardService;
@@ -164,4 +171,25 @@ public class MemberController {
         return R.ok().put("data", classInfoList);
     }
 
+    @PostMapping("/modifyMemberImg.do")
+    @ResponseBody
+    public R modifyMemberImg(Long id, MultipartFile avatarFile) {
+        if (!avatarFile.isEmpty()) {
+            log.debug("\n==>文件上传...");
+            String fileName = UploadImg.uploadImg(avatarFile, UPLOAD_IMAGES_MEMBER_IMG);
+            if (fileName == null) {
+                return R.error("头像上传失败");
+            }
+            if (StringUtils.isNotBlank(fileName)) {
+                MemberEntity member = new MemberEntity().setId(id).setAvatarUrl(fileName).setVersion(+1);
+                boolean isUpdateAvatarUrl = memberService.updateById(member);
+                log.debug("\n==>更新头像是否成功==>{}", isUpdateAvatarUrl);
+                return new R().put("data", member);
+            } else {
+                return R.error("头像上传失败");
+            }
+
+        }
+        return R.error("头像未上传");
+    }
 }
