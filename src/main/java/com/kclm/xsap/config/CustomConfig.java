01c7bd958105b7f53d6e4f7d0b2b48113882c1f0
asgard <1792181243@qq.com>
2023-08-25 17:44:19 +0800

    CustomConfig

--- src/main/java/com/kclm/xsap/config/CustomConfig.java
+++ src/main/java/com/kclm/xsap/config/CustomConfig.java
@@ -1,35 +1,30 @@
 package com.kclm.xsap.config;
 
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+@Getter
 @Component
 @PropertySource("classpath:xsap-config.properties")
 public class CustomConfig {
 
 	@Value("${reservation.gap_minute}")
-	private Long gap_minute;
+	private Long gapMinute;
 
 	@Value("${custom.cache_time}")
-	private Long cache_time;
+	private Long cacheTime;
 
 	public CustomConfig() {
 		super();
 	}
 
-	public Long getGap_minute() {
-		return gap_minute;
-	}
-	public Long getCache_time() {
-		return cache_time;
+	public void setGapMinute(Long gapMinute) {
+		this.gapMinute = gapMinute;
 	}
 
-	public void setGap_minute(Long gap_minute) {
-		this.gap_minute = gap_minute;
-	}
-
-	public void setCache_time(Long cache_time) {
-		this.cache_time = cache_time;
+	public void setCacheTime(Long cacheTime) {
+		this.cacheTime = cacheTime;
 	}
 }
