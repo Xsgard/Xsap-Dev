ddaa045f2637e11eb4719ca759b9b8d72e27752f
asgard <1792181243@qq.com>
2023-08-25 18:01:45 +0800

    EmployeeServiceImpl

--- src/main/java/com/kclm/xsap/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/kclm/xsap/service/impl/EmployeeServiceImpl.java
@@ -1,40 +1,23 @@
 package com.kclm.xsap.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.query.Query;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.kclm.xsap.dao.EmployeeDao;
+import com.kclm.xsap.entity.EmployeeEntity;
 import com.kclm.xsap.exceptions.BusinessException;
+import com.kclm.xsap.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
-
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-
-import com.kclm.xsap.dao.EmployeeDao;
-import com.kclm.xsap.entity.EmployeeEntity;
-import com.kclm.xsap.service.EmployeeService;
-import org.springframework.ui.Model;
 
 
 @Slf4j
 @Service("employeeService")
 public class EmployeeServiceImpl extends ServiceImpl<EmployeeDao, EmployeeEntity> implements EmployeeService {
 
-    /*
-    @Override
-    public PageUtils queryPage(Map<String, Object> params) {
-        IPage<EmployeeEntity> page = this.page(
-                new Query<EmployeeEntity>().getPage(params),
-                new QueryWrapper<EmployeeEntity>()
-        );
-
-        return new PageUtils(page);
-    }
-    */
     @Override
     public EmployeeEntity isExistEmp(String username, String password) {
         EmployeeEntity selectOneForLogin = this.baseMapper.selectOne(new QueryWrapper<EmployeeEntity>().eq("name", username).eq("role_password", password));
