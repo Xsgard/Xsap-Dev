8e47b3ba18c9c9babb368dab8dab4afc58769375
asgard <1792181243@qq.com>
2023-08-25 18:30:14 +0800

    ReservationRecordServiceImpl

--- src/main/java/com/kclm/xsap/service/impl/ReservationRecordServiceImpl.java
+++ src/main/java/com/kclm/xsap/service/impl/ReservationRecordServiceImpl.java
@@ -76,6 +76,7 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
     @Override
     @Transactional
     public void addReserve(ReservationRecordEntity reservationRecord) {
+        String saveFail = "保存失败，请联系管理员！";
         //会员卡绑定实体
         LambdaQueryWrapper<MemberBindRecordEntity> queryWrapper = new LambdaQueryWrapper<>();
         queryWrapper.eq(MemberBindRecordEntity::getMemberId, reservationRecord.getMemberId())
@@ -93,32 +94,14 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
         //校验卡是否激活
         if (memberBindRecord.getActiveStatus() != 1)
             throw new BusinessException("该卡未激活，不能进行消费！");
-
+        //
         LocalDateTime valDay = TimeUtil.timeSubDays(memberBindRecord.getCreateTime(), memberBindRecord.getValidDay());
-        //校验本卡是否支持该课程
-        LambdaQueryWrapper<CourseCardEntity> qw = new LambdaQueryWrapper<>();
-        qw.eq(CourseCardEntity::getCourseId, courseEntity.getId());
-        List<CourseCardEntity> courseCard = courseCardService.list(qw);
-        long count = courseCard.stream()
-                .filter(item -> Objects.equals(item.getCardId(), reservationRecord.getCardId()))
-                .count();
-        if (count < 1)
-            throw new BusinessException("该卡不支持本课程，请换一张会员卡！");
-        //校验卡是否在有效期
-        if (valDay.isBefore(LocalDateTime.now()))
-            throw new BusinessException("该卡已不在有效期！");
-        //校验剩余课次是否足够消费
-        if ((memberBindRecord.getValidCount() - courseEntity.getTimesCost()) < 0) {
-            throw new BusinessException("该卡余额不足以消费该课程！");
-        }
+        //验证卡类型是否满足
+        checkIsMeetCardType(reservationRecord, courseEntity);
+        //会员卡信息校验
+        cardValidation(valDay, memberBindRecord, courseEntity);
         //判断会员是否满足课程要求
-        //课程性别限制
-        if (!courseEntity.getLimitSex().equals("无限制") && !courseEntity.getLimitSex().equals(member.getSex()))
-            throw new BusinessException("很抱歉，您不满足课程的性别要求！");
-        //课程年龄限制
-        int age = member.getBirthday().until(LocalDate.now()).getYears();
-        if (age < courseEntity.getLimitAge())
-            throw new BusinessException("很抱歉，您不满足课程的年龄要求");
+        checkIsMeetCourseRequire(courseEntity, member);
 
         //全局设置实体信息
         GlobalReservationSetEntity globalSet = reservationSetService.getById(1);
@@ -127,29 +110,9 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
         LocalDateTime previousDateTime = TimeUtil.timeMinusDay(startTime, globalSet.getStartDay());
         LocalDateTime now = LocalDateTime.now();
         //判断课程是否已经开始
-        if (now.isAfter(startTime))
-            throw new BusinessException("已经开始上课了，不能预约！");
-        //预约开始时间
-        if (globalSet.getAppointmentStartMode() == 1) {
-            if (now.isAfter(startTime))
-                throw new BusinessException("已经开始上课了，不能预约！");
-        } else {
-            if (now.isBefore(previousDateTime))
-                throw new BusinessException("还未到预约开放日期！");
-        }
-        //预约截止时间
-        if (globalSet.getAppointmentDeadlineMode() == 2) {
-            LocalDateTime endTime = TimeUtil.timeMinusHour(startTime, globalSet.getEndHour());
-            if (now.isAfter(endTime)) {
-                throw new BusinessException("已经过了最晚预约日期！");
-            }
-        } else if (globalSet.getAppointmentDeadlineMode() == 3) {
-            LocalDateTime endDay = TimeUtil.timeTransfer(LocalDate.from(
-                    TimeUtil.timeMinusDay(startTime, globalSet.getEndDay())), globalSet.getEndTime());
-            if (now.isAfter(endDay)) {
-                throw new BusinessException("已经过了最晚预约日期！");
-            }
-        }
+        checkCourseIsStart(now, startTime, globalSet, previousDateTime);
+        //预约截止时间校验
+        checkReserveIsEnd(globalSet, startTime, now);
         //是否预约过该课程
         LambdaQueryWrapper<ReservationRecordEntity> wrapper = new LambdaQueryWrapper<>();
         wrapper.eq(ReservationRecordEntity::getMemberId, reservationRecord.getMemberId())
@@ -158,14 +121,7 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
         ReservationRecordEntity one = this.getOne(wrapper);
         //flag 为true 查询到预约记录
         boolean flag = one != null;
-        //已经有预约记录
-        if (one != null && one.getStatus() == 1) {
-            throw new BusinessException("您已预约过该课程！");
-        }
-        //取消次数超过限制的
-        if (one != null && one.getCancelTimes() >= courseEntity.getLimitCounts()) {
-            throw new BusinessException("您有过多次重复预约并取消本课程的操作，已限制您继续预约该课程！");
-        }
+        checkReservations(one, courseEntity);
         //课堂容量是否足够
         if (reservationRecord.getReserveNums() > (courseEntity.getContains() - scheduleRecord.getOrderNums()))
             throw new BusinessException("该课程太过火爆，已经预约不了了！");
@@ -176,7 +132,7 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
         scheduleRecord.setOrderNums(scheduleRecord.getOrderNums() + reservationRecord.getReserveNums());
         boolean saved = scheduleRecordService.updateById(scheduleRecord);
         if (!saved)
-            throw new BusinessException("保存失败，请联系管理员！");
+            throw new BusinessException(saveFail);
         //查询课程记录信息
         LambdaQueryWrapper<ClassRecordEntity> recordWrapper = new LambdaQueryWrapper<>();
         recordWrapper.eq(ClassRecordEntity::getMemberId, reservationRecord.getMemberId())
@@ -188,7 +144,7 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
             recordEntity.setLastModifyTime(LocalDateTime.now());
             boolean save = classRecordService.updateById(recordEntity);
             if (!save)
-                throw new BusinessException("保存失败，请联系管理员！");
+                throw new BusinessException(saveFail);
         } else {
             //课程记录实体信息
             ClassRecordEntity classRecordEntity = new ClassRecordEntity();
@@ -203,7 +159,7 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
             //保存课程实体信息
             boolean save = classRecordService.save(classRecordEntity);
             if (!save)
-                throw new BusinessException("保存失败，请联系管理员！");
+                throw new BusinessException(saveFail);
         }
 
         //保存预约记录
@@ -216,15 +172,89 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
             b = this.updateById(one);
         }
         if (!b) {
-            throw new BusinessException("保存失败，请联系管理员！");
+            throw new BusinessException(saveFail);
         }
 
+    }
+
+    private static void checkReservations(ReservationRecordEntity one, CourseEntity courseEntity) {
+        //已经有预约记录
+        if (one != null && one.getStatus() == 1) {
+            throw new BusinessException("您已预约过该课程！");
+        }
+        //取消次数超过限制的
+        if (one != null && one.getCancelTimes() >= courseEntity.getLimitCounts()) {
+            throw new BusinessException("您有过多次重复预约并取消本课程的操作，已限制您继续预约该课程！");
+        }
+    }
+
+    private static void checkReserveIsEnd(GlobalReservationSetEntity globalSet, LocalDateTime startTime, LocalDateTime now) {
+        //预约截止时间校验
+        if (globalSet.getAppointmentDeadlineMode() == 2) {
+            LocalDateTime endTime = TimeUtil.timeMinusHour(startTime, globalSet.getEndHour());
+            if (now.isAfter(endTime)) {
+                throw new BusinessException("已经过了最晚预约日期！");
+            }
+        } else if (globalSet.getAppointmentDeadlineMode() == 3) {
+            LocalDateTime endDay = TimeUtil.timeTransfer(LocalDate.from(
+                    TimeUtil.timeMinusDay(startTime, globalSet.getEndDay())), globalSet.getEndTime());
+            if (now.isAfter(endDay)) {
+                throw new BusinessException("已经过了最晚预约日期！");
+            }
+        }
+    }
+
+    private static void checkCourseIsStart(LocalDateTime now, LocalDateTime startTime, GlobalReservationSetEntity globalSet, LocalDateTime previousDateTime) {
+        //判断课程是否已经开始
+        if (now.isAfter(startTime))
+            throw new BusinessException("已经开始上课了，不能预约！");
+        //预约开始时间
+        if (globalSet.getAppointmentStartMode() == 1) {
+            if (now.isAfter(startTime))
+                throw new BusinessException("已经开始上课了，不能预约！");
+        } else {
+            if (now.isBefore(previousDateTime))
+                throw new BusinessException("还未到预约开放日期！");
+        }
+    }
+
+    private static void cardValidation(LocalDateTime valDay, MemberBindRecordEntity memberBindRecord, CourseEntity courseEntity) {
+        //校验卡是否在有效期
+        if (valDay.isBefore(LocalDateTime.now()))
+            throw new BusinessException("该卡已不在有效期！");
+        //校验剩余课次是否足够消费
+        if ((memberBindRecord.getValidCount() - courseEntity.getTimesCost()) < 0) {
+            throw new BusinessException("该卡余额不足以消费该课程！");
+        }
+    }
+
+    private void checkIsMeetCardType(ReservationRecordEntity reservationRecord, CourseEntity courseEntity) {
+        //校验本卡是否支持该课程
+        LambdaQueryWrapper<CourseCardEntity> qw = new LambdaQueryWrapper<>();
+        qw.eq(CourseCardEntity::getCourseId, courseEntity.getId());
+        List<CourseCardEntity> courseCard = courseCardService.list(qw);
+        long count = courseCard.stream()
+                .filter(item -> Objects.equals(item.getCardId(), reservationRecord.getCardId()))
+                .count();
+        if (count < 1)
+            throw new BusinessException("该卡不支持本课程，请换一张会员卡！");
+    }
+
+    private static void checkIsMeetCourseRequire(CourseEntity courseEntity, MemberEntity member) {
+        //课程性别限制
+        if (!courseEntity.getLimitSex().equals("无限制") && !courseEntity.getLimitSex().equals(member.getSex()))
+            throw new BusinessException("很抱歉，您不满足课程的性别要求！");
+        //课程年龄限制
+        int age = member.getBirthday().until(LocalDate.now()).getYears();
+        if (age < courseEntity.getLimitAge())
+            throw new BusinessException("很抱歉，您不满足课程的年龄要求");
     }
 
     //加上取消时间判断是否课程已经开始
     @Override
     @Transactional
     public void cancelReserve(Long reserveId) {
+        String saveFail = "保存失败！";
         ReservationRecordEntity reservationRecord = this.getById(reserveId);
         reservationRecord.setStatus(0);
         reservationRecord.setCancelTimes(reservationRecord.getCancelTimes() + 1);
@@ -259,14 +289,14 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
         recordEntity.setReserveCheck(0);
         boolean updated = classRecordService.updateById(recordEntity);
         if (!updated)
-            throw new BusinessException("保存失败！");
+            throw new BusinessException(saveFail);
         boolean flag = scheduleRecordService.updateById(scheduleRecord);
         if (!flag) {
-            throw new BusinessException("保存失败！");
+            throw new BusinessException(saveFail);
         }
         boolean b = this.updateById(reservationRecord);
         if (!b) {
-            throw new BusinessException("保存失败！");
+            throw new BusinessException(saveFail);
         }
     }
 
@@ -279,11 +309,7 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
     public void exportReservationListToLocal(LocalDate startDate, LocalDate endDate) {
         //
         List<ExportReservationDTO> collect = getExportReservationDTOS();
-        try {
-            ExcelExportUtil.excelWriteToLocal(collect);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        ExcelExportUtil.excelWriteToLocal(collect);
     }
 
     @Override
@@ -293,7 +319,7 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
         try {
             ExcelExportUtil.excelWriteToOnline(collect, response);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new BusinessException("导出失败！");
         }
     }
 
@@ -305,7 +331,7 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
     private List<ExportReservationDTO> getExportReservationDTOS() {
         List<ReservationRecordEntity> recordEntityList = reservationRecordService.list();
         //
-        List<ExportReservationDTO> collect = recordEntityList.stream().map(item -> {
+        return recordEntityList.stream().map(item -> {
             ScheduleRecordEntity schedule = scheduleRecordService.getById(item.getScheduleId());
             CourseEntity course = courseService.getById(schedule.getCourseId());
             MemberEntity member = memberService.getById(item.getMemberId());
@@ -326,7 +352,6 @@ public class ReservationRecordServiceImpl extends ServiceImpl<ReservationRecordD
             dto.setReserveStatus(item.getStatus() == 1 ? "有效" : "无效");
             return dto;
         }).collect(Collectors.toList());
-        return collect;
     }
 
 
