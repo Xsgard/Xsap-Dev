1e7e4ef79ad2bee1c042913e15b7b5509c408f13
asgard <1792181243@qq.com>
2023-08-28 09:23:18 +0800

    CourseCardServiceImpl

--- src/main/java/com/kclm/xsap/service/impl/CourseCardServiceImpl.java
+++ src/main/java/com/kclm/xsap/service/impl/CourseCardServiceImpl.java
@@ -31,23 +31,47 @@ public class CourseCardServiceImpl extends ServiceImpl<CourseCardDao, CourseCard
         return courseCardDao.getCourseIdList(cardId);
     }
 
+    /**
+     * 添加课程会员卡绑定
+     *
+     * @param entities 课程会员卡绑定实体
+     * @return true or false
+     */
     @Override
     public boolean insertCourseCard(List<CourseCardEntity> entities) {
         entities.forEach(item -> courseCardDao.insertCourseCard(item.getCardId(), item.getCourseId()));
         return true;
     }
 
+    /**
+     * 删除课程-会员卡绑定
+     *
+     * @param cardId 会员卡Id
+     * @return true or false
+     */
     @Override
     public boolean deleteCourseCard(Long cardId) {
         int i = courseCardDao.deleteCourseCard(cardId);
         return i > 0;
     }
 
+    /**
+     * 查找课程对应可消费的会员卡
+     *
+     * @param courseId 课程Id
+     * @return 课程Id集合
+     */
     @Override
     public List<Long> getCardIdList(Long courseId) {
         return courseCardDao.getCardIdList(courseId);
     }
 
+    /**
+     * 删除课程-会员卡绑定
+     *
+     * @param id 课程Id
+     * @return true or false
+     */
     @Override
     public boolean deleteCardCourse(Long id) {
         int i = courseCardDao.deleteCardCourse(id);
